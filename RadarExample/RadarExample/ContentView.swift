4536d5f2b3a03f1c3a98d21967728fce07836848
ThaisBras <thaisbras70@icloud.com>
2021-11-03 16:26:45 -0700

    Criar o projeto no Xcode.

---
+++ RadarExample/RadarExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RadarExample
+//
+//  Created by Thais da Silva Bras on 03/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RadarExample/RadarExample/RadarExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadarExampleApp.swift
+//  RadarExample
+//
+//  Created by Thais da Silva Bras on 03/11/21.
+//
+
+import SwiftUI
+
+@main
+struct RadarExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
