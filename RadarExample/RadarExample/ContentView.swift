e5d285125f49f946ae8047607046a27b4ebe39e7
ThaisBras <thaisbras70@icloud.com>
2021-11-03 17:30:42 -0700

    Explicar o funcionamento do gráfico.

--- RadarExample/RadarExample/ContentView.swift
+++ RadarExample/RadarExample/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Rectangle()
+                        .frame(height: 300)
+                    Text("Sobre o gráfico de radar")
+                        .font(.headline)
+                        .padding()
+                    Text("É um método gráfico de apresentar dados multivariáveis, na forma de um gráfico unidimensional com três ou mais variáveis quantitativas, representadas em eixos que partem de um mesmo ponto.")
+                        .padding()
+                    Text("Para obter-se o gráfico é necessário inserir apenas os valores de cada indicador.")
+                        .padding()
+
+                }
+            } .navigationTitle("Gráfico de Radar")
+        }
     }
 }
 
